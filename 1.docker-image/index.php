f38032e46f7abe2ea88e07db24e16093875a1a4e
svdevops07 <svdevops07@gmail.com>
2022-06-14 13:53:37 +0000

    update index.php for version 2

--- 1.docker-image/index.php
+++ 1.docker-image/index.php
@@ -2,6 +2,7 @@
 $ip_server = $_SERVER['SERVER_ADDR'];
 
 echo "<h1>Hello from Kubernetes!</h1><br>";
+echo "<h2>It is version 2</h2>";
 echo "Server IP Address is: $ip_server", "<br><p>";
 echo "Made by <font color=blue>Serjio Borodulin";
 
